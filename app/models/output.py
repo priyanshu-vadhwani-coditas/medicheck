3b78d6c0631ff644ec70e905efc6d7d59504fd3f
Priyanshu Vadhwani <priyanshu.vadhwani@coditas.in>
2025-07-03 19:23:21 +0530

    added model for output parser

---
+++ app/models/output.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class GuardrailOutput(BaseModel):
+    is_insurance_summary: bool
+    reason: str
+    polite_message: str
+
+class PolicyEvalOutput(BaseModel):
+    policy_approved: bool
+    failed_criteria: list[str]
+    policy_message: str 
